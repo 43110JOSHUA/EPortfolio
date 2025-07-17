2db451a670506da4a107f8a638f3d548220543e9
43110JOSHUA <joshuaatang@gmail.com>
2025-07-17 15:05:54 +1000

    Finalizing look of header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,24 @@
 const Header = () => {
   return (
-    <header className="row bg-light text-black p-3 border-bottom">
+    <header className="row bg-light text-black p-2 border-bottom">
       <div className="d-flex justify-content-between align-items-center">
-        <h1>Joshua Tang</h1>
-
-        <button
-          type="button"
-          className="btn btn-link text-black p-0 p-md-2 "
-          style={{ fontSize: "1.5rem" }}
-        >
-          <i className="bi bi-moon"></i>
-        </button>
+        <h2 className="px-2">Joshua Tang</h2>
+        <div className="d-flex">
+          <button
+            type="button"
+            className="btn text-black px-2 d-none d-md-block"
+            style={{ fontSize: "1rem", border: "none" }}
+          >
+            FAQ
+          </button>
+          <button
+            type="button"
+            className="btn text-black px-2"
+            style={{ fontSize: "1.5rem", border: "none" }}
+          >
+            <i className="bi bi-moon"></i>
+          </button>
+        </div>
       </div>
     </header>
   );
