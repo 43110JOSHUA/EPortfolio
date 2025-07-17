9697299f06311b0e52c7741b58d1054cf9a60bfb
43110JOSHUA <joshuaatang@gmail.com>
2025-07-17 12:10:16 +1000

    Moved Header into seperate module

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className="row bg-primary text-white p-4">
+      <div className="d-flex justify-content-between align-items-center">
+        <h1>Joshua Tang</h1>
+        <button className="">test</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
