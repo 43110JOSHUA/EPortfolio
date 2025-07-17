41a33754e1f024efa7ee1fef5b5799eff160b865
43110JOSHUA <joshuaatang@gmail.com>
2025-07-17 14:18:14 +1000

    Improved Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
 const Header = () => {
   return (
-    <header className="row bg-primary text-white p-4">
+    <header className="row bg-light text-black p-3 border-bottom">
       <div className="d-flex justify-content-between align-items-center">
         <h1>Joshua Tang</h1>
-        <button type="button" className="btn btn-link text-white">
+
+        <button
+          type="button"
+          className="btn btn-link text-black p-0 p-md-2 "
+          style={{ fontSize: "1.5rem" }}
+        >
           <i className="bi bi-moon"></i>
         </button>
       </div>
