712b2975e0b992dc4288f46d716102a1a09cf073
43110JOSHUA <joshuaatang@gmail.com>
2025-07-17 12:20:07 +1000

    Moon icon button made

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,9 @@ const Header = () => {
     <header className="row bg-primary text-white p-4">
       <div className="d-flex justify-content-between align-items-center">
         <h1>Joshua Tang</h1>
-        <button className="">test</button>
+        <button type="button" className="btn btn-link text-white">
+          <i className="bi bi-moon"></i>
+        </button>
       </div>
     </header>
   );
