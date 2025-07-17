0c5ecf5403a119bf066290a8689313d3b1d1cf56
43110JOSHUA <joshuaatang@gmail.com>
2025-07-17 15:51:23 +1000

    Added profile image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,28 @@
+import profileImg from "../assets/IMG_3676.jpg";
+
 const Header = () => {
   return (
-    <header className="row bg-light text-dark p-2 border-bottom">
+    <header className="row bg-light text-dark py-2 border-bottom">
       <div className="d-flex justify-content-between align-items-center">
-        <h3 className="">Joshua Tang</h3>
+        <div className="d-flex gap-2">
+          <button
+            type="button"
+            className="btn p-0 rounded-circle overflow-hidden"
+            style={{
+              width: "40px",
+              height: "40px",
+              border: "none",
+              background: "none",
+            }}
+          >
+            <img
+              src={profileImg}
+              alt="Profile"
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            />
+          </button>
+          <h3 className="pt-1">Joshua Tang</h3>
+        </div>
         <div className="d-flex gap-2">
           <button
             type="button"
