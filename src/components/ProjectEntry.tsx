5a03b9fa70552de8752e0422efe1deb4fd31fb76
43110JOSHUA <joshuaatang@gmail.com>
2025-07-19 00:08:29 +1000

    Added hover effect and more formatting

--- src/components/ProjectEntry.tsx
+++ src/components/ProjectEntry.tsx
@@ -1,18 +1,23 @@
 const ProjectEntry = () => {
   return (
-    <div className="card">
-      <img className="card-img-top" />
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card’s content. Some quick example text to build on the
-          card title and make up the bulk of the card’s content. Some quick
-          example text to build on the card title and make up the bulk of the
-          card’s content.
-        </p>
+    <button className="btn text-start p-0">
+      <div className="card">
+        <div className="card-body">
+          <div className="d-flex justify-content-between align-items-center">
+            <h5 className="text-primary">Card title</h5>
+            <h6>2025</h6>
+          </div>
+
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card’s content. Some quick example text to build on the
+            card title and make up the bulk of the card’s content. Some quick
+            example text to build on the card title and make up the bulk of the
+            card’s content.
+          </p>
+        </div>
       </div>
-    </div>
+    </button>
   );
 };
 
