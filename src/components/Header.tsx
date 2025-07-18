56939bd264cf69dc341e24b1d54b543b9c9c045a
43110JOSHUA <joshuaatang@gmail.com>
2025-07-19 00:33:40 +1000

    Fixed avatar shrinking on very small devices

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = ({ toggleTheme }: HeaderProps) => {
   return (
     <header className="row bg-body-tertiary border-bottom sticky-top py-2 px-2   px-md-3">
       <div className="d-flex justify-content-between align-items-center">
-        <div className="d-flex gap-2 align-items-center">
+        <div className="d-flex d-wrap gap-2 align-items-center">
           <Avatar src={profileImg} />
           <h5 className="pt-1 px-2">Joshua Tang / EPortfolio</h5>
         </div>
