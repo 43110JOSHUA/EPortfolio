e1f43a7ad9870d2fb5521416de644b9a4d7b97f3
43110JOSHUA <joshuaatang@gmail.com>
2025-07-19 17:36:26 +1000

    Formatting ExperienceEntry component

---
+++ src/components/ExperienceEntry.tsx
@@ -0,0 +1,22 @@
+const ExperienceEntry = () => {
+  return (
+    <li className="list-group-item p-3">
+      <div className="d-flex justify-content-between align-items-top gap-4">
+        <h6>Role - Microsoft</h6>
+        <h6 className="text-end">Sep 2025 - Dec 2025</h6>
+      </div>
+      <div>
+        <ul>
+          <li>Developed new features for the company website</li>
+          <li>Collaborated with the design team to improve user experience</li>
+          <li>
+            Participated in code reviews and contributed to team knowledge
+            sharing
+          </li>
+        </ul>
+      </div>
+    </li>
+  );
+};
+
+export default ExperienceEntry;
