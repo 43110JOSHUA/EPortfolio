0f297c3b0a66a6ad7528b2b34848a8ac8cc653ed
43110JOSHUA <joshuaatang@gmail.com>
2025-07-19 16:20:51 +1000

    Added Props to Project Entry component

--- src/components/ProjectEntry.tsx
+++ src/components/ProjectEntry.tsx
@@ -1,23 +1,27 @@
-const ProjectEntry = () => {
+interface Props {
+  title: string;
+  year: string;
+  tools: string[];
+  children: React.ReactNode;
+}
+
+const ProjectEntry = ({ title, year, tools, children }: Props) => {
   return (
     <button className="btn text-start p-0">
       <div className="card">
         <div className="card-body">
           <div className="d-flex justify-content-between align-items-center">
-            <h5 className="text-primary">Card title</h5>
-            <h6>2025</h6>
+            <h5 className="text-primary">{title}</h5>
+            <h6>{year}</h6>
           </div>
 
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card’s content. Some quick example text to build on the
-            card title and make up the bulk of the card’s content. Some quick
-            example text to build on the card title and make up the bulk of the
-            card’s content.
-          </p>
+          <p className="card-text">{children}</p>
           <div className="container-fluid d-flex flex-wrap gap-1 p-0">
-            <span className="badge rounded-pill text-bg-warning">Python</span>
-            <span className="badge rounded-pill text-bg-success">React</span>
+            {tools.map((tool, index) => (
+              <span key={index} className="badge rounded-pill text-bg-warning">
+                {tool}
+              </span>
+            ))}
           </div>
         </div>
       </div>
--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -6,13 +6,39 @@ const ProjectList = () => {
       <h5 className="text-center pb-2">My Projects</h5>
       <ul className="list-group">
         <li className="list-group-item d-flex flex-column gap-3 p-3">
-          <ProjectEntry />
-
-          <ProjectEntry />
-
-          <ProjectEntry />
-
-          <ProjectEntry />
+          <ProjectEntry
+            title="Charity Code"
+            year="2025"
+            tools={["Python", "React"]}
+          >
+            Some quick example text to build on the card title and make up the
+            bulk of the card’s content. Some quick example text to build on the
+            card title and make up the bulk of the card’s content. Some quick
+            example text to build on the card title and make up the bulk of the
+            card’s content.
+          </ProjectEntry>
+          <ProjectEntry
+            title="Portfolio Website"
+            year="2025"
+            tools={["React", "TypeScript", "Bootstrap"]}
+          >
+            Some quick example text to build on the card title and make up the
+            bulk of the card’s content. Some quick example text to build on the
+            card title and make up the bulk of the card’s content. Some quick
+            example text to build on the card title and make up the bulk of the
+            card’s content.
+          </ProjectEntry>
+          <ProjectEntry
+            title="Class Tracker"
+            year="2025"
+            tools={["Python", "SQLite"]}
+          >
+            Some quick example text to build on the card title and make up the
+            bulk of the card’s content. Some quick example text to build on the
+            card title and make up the bulk of the card’s content. Some quick
+            example text to build on the card title and make up the bulk of the
+            card’s content.
+          </ProjectEntry>
         </li>
       </ul>
     </>
