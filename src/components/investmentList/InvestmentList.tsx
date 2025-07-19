8a98f82ceaf596962044337031b75a9e069686b0
43110JOSHUA <joshuaatang@gmail.com>
2025-07-19 22:02:23 +1000

    Added avg cost per share to var

--- src/components/investmentList/InvestmentList.tsx
+++ src/components/investmentList/InvestmentList.tsx
@@ -1,16 +1,16 @@
-const investments: [number, string][] = [
-  [58, "AMZN"],
-  [32, "AFRM"],
-  [30, "GOOGL"],
-  [23, "AAPL"],
-  [15, "MSFT"],
-  [9, "SN"],
-  [6, "META"],
-  [4, "SPGI"],
-  [1, "ASML"],
-  [1, "COST"],
-  [1, "INTU"],
-]; // Add More Investments Here
+const investmentsCost: [number, string, number][] = [
+  [23, "AAPL", 176.58],
+  [32, "AFRM", 52.48],
+  [58, "AMZN", 169.77],
+  [1, "ASML", 736.75],
+  [1, "COST", 926.46],
+  [30, "GOOGL", 145.49],
+  [1, "INTU", 600.02],
+  [6, "META", 591.3],
+  [15, "MSFT", 376],
+  [9, "SN", 106.71],
+  [4, "SPGI", 535.05],
+]; // # Shares, Company Symbol, Average Cost per Share
 
 const InvestmentList = () => {
   return (
@@ -19,7 +19,7 @@ const InvestmentList = () => {
       <ul className="list-group">
         <li className="list-group-item d-flex flex-column gap-3 p-3">
           <h5 className="container-fluid justify-content-center d-flex flex-wrap gap-1 p-0">
-            {investments.map(([amount, symbol]) => (
+            {investmentsCost.map(([amount, symbol]) => (
               <span key={symbol} className="badge bg-success">
                 {amount}-{symbol}
               </span>
