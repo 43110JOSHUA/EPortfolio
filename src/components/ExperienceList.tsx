ed3b7a4868a003901f2895b66e8b536050a0c086
43110JOSHUA <joshuaatang@gmail.com>
2025-07-19 17:36:16 +1000

    Formatting ExperienceEntry component

--- src/components/ExperienceList.tsx
+++ src/components/ExperienceList.tsx
@@ -1,11 +1,13 @@
+import ExperienceEntry from "./ExperienceEntry";
+
 const ExperienceList = () => {
   return (
     <>
       <h5 className="text-center pb-2">My Experience</h5>
       <ul className="list-group">
-        <li className="list-group-item">Experience 1</li>
-        <li className="list-group-item">Experience 2</li>
-        <li className="list-group-item">Experience 3</li>
+        <ExperienceEntry />
+        <ExperienceEntry />
+        <ExperienceEntry />
       </ul>
     </>
   );
