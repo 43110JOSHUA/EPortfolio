c591bea48b98f827fe7d65d203fb43c59faf879c
43110JOSHUA <joshuaatang@gmail.com>
2025-07-22 12:05:23 +1000

    Added a live badge

--- src/components/investmentList/InvestmentList.tsx
+++ src/components/investmentList/InvestmentList.tsx
@@ -5,7 +5,12 @@ import InvestmentPrices from "./InvestmentPrices";
 const InvestmentList = () => {
   return (
     <>
-      <h5 className="text-center pb-2">My Investments</h5>
+      <div className="flex-container justify-content-center align-items-center d-flex flex-wrap gap-2 pb-2">
+        <h5 className="text-center">My Investments</h5>
+        <button className="btn btn-outline-secondary disabled mb-2 align-items-center d-flex gap-2">
+          Live <div className="spinner-grow spinner-grow-sm text-danger"></div>
+        </button>
+      </div>
       <ul className="list-group">
         <li className="list-group-item d-flex flex-column gap-3 p-3">
           <InvestmentPrices />
